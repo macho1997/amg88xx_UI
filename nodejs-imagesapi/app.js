fb585346b3579ce9296fbfcaa535630ad6ce54e8
Emilio Soto <carlos97emilio18@gmail.com>
2020-08-13 00:35:26 -0600

    Modified Nodejs backend

--- nodejs-imagesapi/app.js
+++ nodejs-imagesapi/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
 
 app.use(cors());
 
-app.use('/requireimage', express.static(__dirname + '/public/images'));
-app.use('/requirestatus', express.static(__dirname + '/public/status'));
+const dataPath = __dirname + '/status.json';
 
+app.use('/requireimage', express.static(__dirname + '/public/images'));
+
+app.get("/getstatus", (req, res) => {
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(JSON.parse(data));
+    });
+  });
 
 app.listen(3000, () => {
     console.log("Server listening on 3000");
